3f5f96591253ad6aa3f21cdf3519b23f08b1ac8e
MacPherson <maxim.troitsky@gmail.com>
2014-10-26 23:21:45 +0200

    add get request and save image

---
+++ api/screenshots.js
@@ -0,0 +1,8 @@
+var express = require('express');
+var router = express.Router();
+
+router.get('/api/screenshots', function(req, res) {
+    res.send('This is not implemented now');
+});
+
+module.exports = router;
